2379a1e2e7dc8fb78617a3f4a0ce50fa5a00393c
alexzaytsev7 <alexber7@mail.ru>
2023-05-27 13:38:06 +0300

    Add HW8

---
+++ HW8/Ex1.py
@@ -0,0 +1,66 @@
+# Задача 38: Дополнить телефонный справочник возможностью изменения и удаления данных.
+# Пользователь также может ввести имя или фамилию, и Вы должны реализовать функционал для изменения и
+# удаления данных
+
+def add_contact(f):
+    input_name = input('Введите Имя: ')
+    input_last_name = input('Введите Фамилию: ')
+    input_phone = input('Введите номер телефона: ')
+    data = f'{input_last_name}; {input_name}; {input_phone}'
+    with open(f, 'a', encoding='utf-8') as fd:
+        fd.write(f'{data}\n')
+    print(f'Запись {data} добавлена')
+
+
+def read_all(f):
+    with open(f, 'r', encoding='utf-8') as fd:
+        file_list = fd.readlines()
+        return file_list
+
+def print_all(f):
+    adr_book = read_all(f)
+    for line in adr_book:
+        line = line.replace(';', '')
+        line = line.replace('\n', '')
+        print(line)
+
+
+
+def search_contact(f):
+    last_name = input('Введите фамилию для поиска: ')
+    adr_book = read_all(f)
+    #print(len(adr_book))
+    for i in range(len(adr_book)):
+        if last_name in adr_book[i]:
+            print(i, adr_book[i])
+
+    idx = int(input('Введи индекс для замены: '))
+
+    last_name = input('введите новую фамилию: ')
+    name = input('введите новое имя: ')
+    phone = input('новый номер: ')
+    new_record = f'{last_name}; {name}; {phone}'
+    adr_book[idx] = new_record
+    with open(f, 'w', encoding='utf-8') as fd:
+        fd.writelines(adr_book)
+
+
+def main():
+    # user_choice = ''
+    # while user_choice != 'z':
+    file = 'address_book.txt'
+    while True:
+        user_choice = input('1 - добавить запись,\n2 - прочитать всю тел.книгу,\n'
+                            '3 - найти запись,\nz - для выхода: ')
+        if user_choice == '1':
+            add_contact(file)
+        elif user_choice == '2':
+            print_all(file)
+        elif user_choice == '3':
+            search_contact(file)
+        elif user_choice == 'z':
+            print('Всего хорошего')
+            break
+
+if __name__ == '__main__':
+    main()
